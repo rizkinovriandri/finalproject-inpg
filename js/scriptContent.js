75ac879c914f18581ddf9ece6701b965f358bcf0
rizkinovriandri <qieroway@gmail.com>
2020-07-18 13:38:33 +0700

    update file
    Update File

---
+++ js/scriptContent.js
@@ -0,0 +1,559 @@
+var testimoni = [
+    {
+        foto : 'testi-2.jpg',
+        komentar : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor finibus risus. Vivamus quis aliquet nibh. Nunc vitae felis nunc. Nam scelerisque maximus tempor. Proin sed euismod tellus. Nunc sodales, quam et porttitor accumsan, lectus dolor laoreet dolor, a scelerisque.',
+        nama : 'Cristiano Ronaldo',
+        pekerjaan : 'Footballer',
+    },
+
+    {
+        foto : 'testi-4.jpg',
+        komentar : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor finibus risus. Vivamus quis aliquet nibh. Nunc vitae felis nunc. Nam scelerisque maximus tempor. Proin sed euismod tellus. Nunc sodales, quam et porttitor accumsan, lectus dolor laoreet dolor, a scelerisque.',
+        nama : 'Bruno Mars',
+        pekerjaan : 'Singer',
+    },
+
+    {
+        foto : 'testi-6.jpg',
+        komentar : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor finibus risus. Vivamus quis aliquet nibh. Nunc vitae felis nunc. Nam scelerisque maximus tempor. Proin sed euismod tellus. Nunc sodales, quam et porttitor accumsan, lectus dolor laoreet dolor, a scelerisque.',
+        nama : 'Pevita Pearce',
+        pekerjaan : 'Actress',
+    },
+
+    {
+        foto : 'testi-1.jpg',
+        komentar : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor finibus risus. Vivamus quis aliquet nibh. Nunc vitae felis nunc. Nam scelerisque maximus tempor. Proin sed euismod tellus. Nunc sodales, quam et porttitor accumsan, lectus dolor laoreet dolor, a scelerisque.',
+        nama : 'Lionel Messi',
+        pekerjaan : 'Footballer',
+    },
+
+    {
+        foto : 'testi-5.jpg',
+        komentar : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor finibus risus. Vivamus quis aliquet nibh. Nunc vitae felis nunc. Nam scelerisque maximus tempor. Proin sed euismod tellus. Nunc sodales, quam et porttitor accumsan, lectus dolor laoreet dolor, a scelerisque.',
+        nama : 'Elon Musk',
+        pekerjaan : 'CEO SpaceX',
+    },
+
+    {
+        foto : 'testi-3.jpg',
+        komentar : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor finibus risus. Vivamus quis aliquet nibh. Nunc vitae felis nunc. Nam scelerisque maximus tempor. Proin sed euismod tellus. Nunc sodales, quam et porttitor accumsan, lectus dolor laoreet dolor, a scelerisque.',
+        nama : 'Mesut Ozil',
+        pekerjaan : 'Footballer',
+    }
+
+
+];
+
+
+var team = [
+    {
+        foto : 'team-1.jpg',
+        expertise : 'Interior Expert',
+        nama : 'PETER PARKER',
+        desc : 'Berpengalaman kurang lebih 10 tahun mengerjakan proyek pemerintahan ataupun klien personal',
+    },
+
+    {
+        foto : 'team-2.jpg',
+        expertise : 'Landscaping Expert',
+        nama : 'Tony Stark',
+        desc : 'Beberapa tahun terakhir banyak mengerjakan project-project pembuatan taman kota.',
+    },
+
+    {
+        foto : 'team-3.jpg',
+        expertise : 'Sanitary Expert',
+        nama : 'Bruce wayne',
+        desc : 'Memiliki keahlian tinggi dalam membersihkan saluran air',
+    },
+
+    {
+        foto : 'team-4.jpg',
+        expertise : 'Lead Consultant',
+        nama : 'Bruce Banner',
+        desc : 'Berpengalaman dalam mengerjakan proyek-proyek interior minimalis',
+    },
+
+    {
+        foto : 'team-5.jpg',
+        expertise : 'Lighting Expert',
+        nama : 'Scott Lang',
+        desc : 'Konsultan ahli pencahayaan. Scott telah beberapa kali turut serta dalam proyek strategis pemerintah',
+    },
+
+    {
+        foto : 'team-6.jpg',
+        expertise : 'Material Expert',
+        nama : 'Natasha Romanoff',
+        desc : 'Konsultan ahli material, Berpengalaman dalam mengerjakan proyek-proyek interior',
+    },
+    
+];
+
+var partner = [
+
+     'partners-1.png'
+    ,
+
+     'partners-2.png'
+    ,
+
+     'partners-3.png'
+    ,
+
+     'partners-4.png'
+    ,
+
+     'partners-5.png'
+    ,
+
+     'partners-6.png'
+    ,
+
+     'partners-7.png'
+    ,
+
+     'partners-8.png'
+    ,
+
+     'partners-9.png'
+    ,
+
+     'partners-10.png'
+    ,
+
+
+]
+
+var portfolio = [
+    {
+        foto : 'portfolio-1.jpg',
+        title : 'Fasad Gedung',
+        desc : 'Coworking Space Medan',
+    },
+
+    {
+        foto : 'portfolio-2.jpg',
+        title : 'Green Living',
+        desc : 'Project Rumah Modern Ramah Lingkungan Medan',
+    },
+
+    {
+        foto : 'portfolio-3.jpg',
+        title : 'Laser Cutting Facade',
+        desc : 'Fasad Aluminium dengan Teknik CNC Laser Cutting',
+    },
+
+    {
+        foto : 'portfolio-4.jpg',
+        title : 'Green Landscape',
+        desc : 'Landscaping Taman Universitas Sumatera Utara',
+    },
+
+    {
+        foto : 'portfolio-5.jpg',
+        title : 'Interior Bank BNI',
+        desc : 'Renovasi Interior kantor Pusat Bank BNI',
+    },
+
+    {
+        foto : 'portfolio-6.jpg',
+        title : 'Luxury Bathroom',
+        desc : 'Bathroom Interior Hotel Mariott Medan',
+    },
+    
+];
+
+var blog = [
+    {
+        img : 'https://akcdn.detik.net.id/community/media/visual/2020/07/17/ilustrasi-kerja-di-kantor.jpeg?w=700&q=90',
+        title : 'Kembali Ngantor di Masa New Normal',
+        author : 'Yudistira Imandiar',
+        date : 'March 07, 2020',
+        highlight : 'Di era New Normal, sejumlah perusahaan dan tempat usaha sudah kembali',
+    },
+
+    {
+        img : 'https://akcdn.detik.net.id/community/media/visual/2020/03/06/6016961f-ccd5-46e5-8f73-d7079bc8405a_169.jpeg?w=700&q=90',
+        title : '5 Cara Mengusir Nyamuk Secara Alami Tanpa Obat Semprot',
+        author : 'Hestianingsih',
+        date : 'Jul 09, 2020',
+        highlight : 'Nyamuk sangat mengganggu kenyamanan terlebih lagi di malam hari ketika menjelang tidur.',
+    },
+
+    {
+        img : 'https://akcdn.detik.net.id/community/media/visual/2017/02/28/c1cdba5e-10e0-4eb9-8fb4-78e3d2b1db54.jpg?w=700&q=90',
+        title : '5 Cara Menghilangkan Noda Luntur Pada Baju Putih',
+        author : 'Lukita Wardani',
+        date : 'Mei 15, 2020',
+        highlight : 'Membersihkan noda pada baju bisa menggunakan roti gandum atau jenis roti lainnya.',
+    },
+
+    {
+        img : 'https://akcdn.detik.net.id/community/media/visual/2020/06/29/kaktus.jpeg?w=700&q=90',
+        title : 'Cara Merawat Kaktus di Rumah yang Penting bagi Pemula',
+        author : 'Lusiana Mustinda',
+        date : 'Jun 30, 2020',
+        highlight : 'Kaktus dan sukulen termasuk dalam kelompok yang sama. Keduanya memiliki karakteristik',
+    },
+
+    
+];
+
+
+
+
+// ------------------ script iterasi testimoni ----------------------
+
+function generateTesti(arr){
+    console.log('panjang array ' + arr.length);
+    var content = "";
+
+    for (let i=0; i<arr.length ; i++){
+        console.log('masuk');
+
+        content += '<div class="testimonial">'+
+                        '<div class="pic">'+
+                            '<img src="images/demo/'+ arr[i].foto + '" alt="'+ arr[i].nama +'">' +
+                                '</div>'+
+                                    '<p class="description">'+
+                                        arr[i].komentar +
+                                        '</p>'+
+                                        '<h3 class="testimonial-title">' + arr[i].nama +' - '+  arr[i].pekerjaan + '</h3>' +
+                                    '</div>';
+
+
+    }
+
+    document.getElementById("testimonial-slider").innerHTML = content;
+}
+
+// --------------- /script iterasi testimoni ---------------------------
+
+
+// ------------------ script iterasi team ----------------------
+
+function generateTeam(arr){
+    console.log('panjang array ' + arr.length);
+    var content = "";
+    let newRow = "";
+    let closeRow = "";
+
+    let count=0;
+
+    for (let i=0; i<arr.length ; i++){
+        //console.log('i = ' + i);
+
+
+        if (count > 2){
+            count = 0;
+        }
+        
+
+        if (count === 0) {
+            newRow = '<div class="row">';
+        }
+
+        //console.log('count = ' + count);
+        //console.log('newRow = ' + newRow);console.log('closerow = ' + closeRow);
+        
+        content += newRow + '<div class="col-md-4">' +
+                        '<div class="team-item">' +
+                            '<div class="team-image">' +
+                                '<img src="images/demo/' + arr[i].foto + '" class="img-responsive" alt="author">' +
+                            '</div>' +
+                            '<div class="team-text">' +
+                                '<h3>' + arr[i].nama + '</h3>' +
+                                '<div class="team-position">' + arr[i].expertise + '</div>'+
+                                '<p>' + arr[i].desc + '</p>'+
+                                '<div class="team-socials">'+
+                                    '<a href="#"><i class="fa fa-twitter"></i></a>'+
+                                    '<a href="#"><i class="fa fa-facebook"></i></a>'+
+                                    '<a href="#"><i class="fa fa-youtube"></i></a>'+
+                                    '<a href="#"><i class="fa fa-google-plus"></i></a>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>';
+
+        if ((count === 2) || ((count != 2) && (i === arr.length-1))){
+            closeRow = '</div><br>';
+            content += closeRow;
+        }
+        
+        count++;
+
+        newRow = "";
+        closeRow = "";
+        
+    }
+
+    document.getElementById("containerTeam").innerHTML = content;
+}
+
+// --------------- /script iterasi team ---------------------------
+
+// ------------------ script iterasi partner ----------------------
+
+function generatePartner(arr){
+    console.log('panjang array ' + arr.length);
+    var content = "";
+
+    //let opener = ''
+
+    for (let i=0; i<arr.length ; i++){
+        console.log('masuk');
+
+        content += '<div class="testimonial">'+
+                        '<div class="pic">'+
+                            '<img src="images/demo/' + arr[i] +'" alt="">'+
+                        '</div>'+
+                    '</div>';
+
+
+    }
+
+    document.getElementById("partner-slider").innerHTML = content;
+}
+
+// --------------- /script iterasi partner ---------------------------
+
+
+
+// ------------------ script iterasi portfolio ----------------------
+
+function generatePortfolio(arr){
+    console.log('panjang array 2 ' + arr.length);
+    var content = "";
+    let newRow = "";
+    let closeRow = "";
+
+    let count=0;
+
+    for (let i=0; i<arr.length ; i++){
+        //console.log('i = ' + i);
+
+
+        if (count > 2){
+            count = 0;
+        }
+        
+
+        if (count === 0) {
+            newRow = '<div class="row row-0-gutter">';
+        }
+
+        //console.log('count = ' + count);
+        //console.log('newRow = ' + newRow);console.log('closerow = ' + closeRow);
+        
+        content += newRow + '<div class="col-md-4 col-0-gutter">' +
+                                '<div class="ot-portfolio-item">' +
+                                    '<figure class="effect-bubba">' +
+                                        '<img src="images/demo/'+ portfolio[i].foto +'" alt="img02" class="img-responsive" />' +
+                                        '<figcaption>' +
+                                            '<h2>'+ portfolio[i].title +'</h2>' +
+                                            '<p>'+ portfolio[i].desc +'</p>' +
+                                            '<a href="#" data-toggle="modal" data-target="#Modal-'+ (i+1) +'">View more</a>' +
+                                        '</figcaption>' +
+                                    '</figure>' +
+                                '</div>' +
+                            '</div>';
+
+        if ((count === 2) || ((count != 2) && (i === arr.length-1))){
+            closeRow = '</div>';
+            content += closeRow;
+        }
+        
+        count++;
+
+        newRow = "";
+        closeRow = "";
+        
+    }
+
+    var contentModal = "";
+
+    for (let i=0; i<arr.length ; i++){
+        console.log('masuk');
+
+        contentModal += '<div class="modal fade" id="Modal-'+ (i+1) +'" tabindex="-1" role="dialog" aria-labelledby="Modal-label-'+ (i+1) +'">'+
+                            '<div class="modal-dialog" role="document">'+
+                                '<div class="modal-content">'+
+                                   '<div class="modal-header">'+
+                                        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                                        '<h4 class="modal-title" id="Modal-label-1">'+ arr[i].title +'</h4>'+
+                                    '</div>'+
+                                    '<div class="modal-body">'+
+                                        '<img src="images/demo/'+ arr[i].foto +'" alt="img01" class="img-responsive" />'+
+                                        '<div class="modal-works"><span>Architecture</span><span>Design</span></div>'+
+                                        '<p>'+ arr[i].desc +'</p>'+
+                                   '</div>'+
+                                   '<div class="modal-footer">'+
+                                        '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'+
+                       ' </div>';
+    }
+
+
+    document.getElementById("containerPortfolio").innerHTML = content;
+    document.getElementById("modalShow").innerHTML = contentModal;
+}
+
+// --------------- /script iterasi portfolio ---------------------------
+
+
+// ------------------ script iterasi team ----------------------
+
+function generateBlog(arr){
+    console.log('panjang array ' + arr.length);
+    var content = "";
+    let newRow = "";
+    let closeRow = "";
+
+    let count=0;
+
+    for (let i=0; i<arr.length ; i++){
+        //console.log('i = ' + i);
+
+
+        if (count > 1){
+            count = 0;
+        }
+        
+
+        if (count === 0) {
+            newRow = '<div class="row">';
+        }
+
+        //console.log('count = ' + count);
+        //console.log('newRow = ' + newRow);console.log('closerow = ' + closeRow);
+        
+        content += newRow + '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" data-aos="fade-right">'+
+                                '<div class="col-lg-6 col-xs-12">'+
+                                    '<img src="'+ arr[i].img +'" alt="" width="100%">'+
+                                '</div>'+
+                                '<div class="col-lg-6 col-xs-12">'+
+                                    '<div class="blog-column">'+
+                                        '<span>'+ arr[i].title +'</span>'+
+                                        '<ul class="blog-detail list-inline">'+ 
+                                            '<li><i class="fa fa-user"></i>'+ arr[i].author +'</li>'+ 
+                                            '<li><i class="fa fa-clock-o"></i>'+ arr[i].date +'</li>'+ 
+                                        '</ul>'+
+                                        '<p>'+ arr[i].highlight +'</p>'+
+                                        '<a href="post.html">Read More</a>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>';
+
+        if ((count === 1) || ((count != 1) && (i === arr.length-1))){
+            closeRow = '</div><br><br>';
+            content += closeRow;
+        }
+        
+        count++;
+
+        newRow = "";
+        closeRow = "";
+        
+    }
+
+    document.getElementById("blogContainer").innerHTML = content;
+}
+
+// --------------- /script iterasi team ---------------------------
+
+
+
+
+if(document.getElementById("containerTesti")!= null){
+    generateTesti(testimoni);
+}
+
+if(document.getElementById("containerTeam")!= null){
+    generatePartner(partner);
+    generateTeam(team);
+    
+}
+
+if(document.getElementById("containerPortfolio")!= null){
+    generatePortfolio(portfolio);
+}
+
+if(document.getElementById("blogContainer")!= null){
+    generateBlog(blog);
+}
+
+function onSubmitKontak(event) {
+    event.preventDefault();
+
+    //get data dari input box user
+    let nama = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    
+    if(nama === ""){
+        alert("Harap isi nama anda!");
+    } else if (email === ""){
+        alert("Harap isi email anda!");
+    } else if (message === ""){
+        alert("Harap isi pesan anda!");
+    } else {
+        alert("Terima kasih Bapak/Ibu " + nama + " atas pesan yang disampaikan.");
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('message').value = "";
+        document.getElementById('name').focus();
+    }
+    
+}
+
+// Comment di Post
+
+let comments = [];
+let names = [];
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    //get data dari input box user
+    let nama = document.getElementById('txtNama').value;
+    let komentar = document.getElementById('txtKomentar').value;
+
+    if(nama === ""){
+        alert("Harap isi nama anda!");
+    } else if (komentar === ""){
+        alert("Harap isi komentar anda!");
+    } else {
+
+        comments.unshift(komentar);
+        names.unshift(nama);
+
+        let data = '';
+
+        for(i = 0 ; i<comments.length ; i++){
+            data += '<li class="list-group-item"><b>' + names[i] + '</b><br><br>'+ comments[i] + '</li>';
+        }
+
+        var jlhKomentar = comments.length;
+        
+        var data2 = "(" + jlhKomentar + ")";
+
+        document.getElementById("listComments").innerHTML = data;
+        document.getElementById("countComment").innerHTML = data2;
+
+        console.log("data");
+        //alert("Terima kasih telah memberikan komentar");
+
+        document.getElementById('txtNama').value = "";
+        document.getElementById('txtKomentar').value = "";
+        document.getElementById('txtNama').focus();
+        //kirim balik data dari js ke html
+    }
+    
+
+}
+
+
+
